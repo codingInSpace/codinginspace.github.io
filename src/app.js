7979afbe250ec5ffeb323fe32d2538171d751254
codingInSpace <jonathan.grangien@gmail.com>
2016-05-08 17:14:48 +0200

    Implement better structure

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Message from './components';
+import Message from './components/message';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
-import reducer from './reducer';
+import reducer from './redux/reducer';
 import { Provider } from 'react-redux';
-//var css = require("./file.scss")
 import "!style!css!sass!./stylesheets/main.scss";
 
 const store = createStore(reducer);
---
+++ src/redux/containers.js
@@ -0,0 +1,2 @@
+import { connect } from 'react-redux';
+import * as components from '../components/message';
