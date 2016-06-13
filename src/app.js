b3b9b0153fa12b526810faca1b103a415704f62c
codingInSpace <jonathan.grangien@gmail.com>
2016-06-14 01:44:24 +0200

    Implement get projects via json request

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
+import * as axios from 'axios';
 
 import {deepPurple500} from 'material-ui/styles/colors';
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -14,6 +15,7 @@ import { createStore } from 'redux';
 import reducer from './redux/reducer';
 import { Provider } from 'react-redux';
 import "!style!css!sass!./stylesheets/main.scss";
+//import * as projects from './components/projects';
 
 const muiTheme = getMuiTheme({
   palette: {
@@ -21,31 +23,57 @@ const muiTheme = getMuiTheme({
   },
 });
 
-var projects = [
-	{
-		id: 1,
-		src: "public/cat1.jpg"
-	},
-	{
-		id: 2,
-		src: "public/cat2.jpg"
-	}
-];
+//var projects = [
+	//{
+		//id: 1,
+		//src: "public/cat1.jpg"
+	//},
+	//{
+		//id: 2,
+		//src: "public/cat2.jpg"
+	//}
+//];
+
+const store = createStore(reducer);
 
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = { data: [{id: 1, src: "public/cat1.jpg"}] };
+	}
+
+	loadProjects() {
+		let that = this;
+		
+		axios.get('src/data/projects.json')
+			.then(function (response) {
+				console.log("got " + response);
+				var newData = that.state.data.concat([response.data]); 
+				that.setState({ data: newData })
+				console.log("new state:" + that.state.data);
+			});
+			//.catch(function (response) {
+				//console.log("caught " + response);
+		//});
+			
+	}
+
+	componentDidMount() {
+		console.log("loading projects");
+		this.loadProjects();
+	}
+
 	render() {
 		return (
 			<MuiThemeProvider muiTheme={muiTheme}>
 			<div>
 				<AppBarHeader/>
-				<ProjectsGallery projects={projects}/>
+				<ProjectsGallery projects={this.state.data}/>
 			</div>
 			</MuiThemeProvider>
 		);
 	}
 }
-
-const store = createStore(reducer);
 
 injectTapEventPlugin();
 ReactDOM.render(
