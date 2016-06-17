1b9513d189d64492242c09756c2c7e199bd8a9c7
codingInSpace <jonathan.grangien@gmail.com>
2016-06-17 02:38:38 +0200

    Customize project elements with title and tag information

--- src/components/ProjectElement.js
+++ src/components/ProjectElement.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 class ProjectElement extends React.Component {
 	render() {
+		const tags = this.props.elem.tags.map(function (tag) {
+			return (
+				<span className="tag">{tag}</span>
+			);
+		});
+
 		return (
 			<li className="project-element">
 				<img src={this.props.elem.src}/>
+				<div className="title">
+					<p>{this.props.elem.title}</p>
+					{tags}
+				</div>
 			</li>
 		);
 	}
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,15 @@
 const projects = [
 	{
 		"id": "1",
-		"src": "/public/cat1.jpg"
+		"src": "/public/cat1.jpg",
+		"title": "A cat",
+		"tags": [ "C++", "OpenGL" ]
 	},
 	{
 		"id": "2",
-		"src": "/public/cat2.jpg"
+		"src": "/public/cat2.jpg",
+		"title": "Another cat",
+		"tags": [ "Web" ]
 	}
 ]
 
