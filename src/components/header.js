61e969b6834f9ac8f78768034d7bd4d45fe44fba
codingInSpace <jonathan.grangien@gmail.com>
2016-06-20 16:00:12 +0200

    Implement full page first page concept

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Col } from 'react-bootstrap';
 
 class Header extends Component {
   render() {
 		return (
 			<header>
-				<Row>
-					<Col sm={6} smOffset={3}>
-						<p>Hej jag heter jonathan :D</p>
-					</Col>
-				</Row>
+				<Col sm={6} smOffset={3}>
+					<p>Hej jag heter jonathan :D</p>
+				</Col>
 			</header>
 		);
 	}
