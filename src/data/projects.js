90f91ab40709cd742b9ab06d9e0f476ff54f5e96
codingInSpace <jonathan.grangien@gmail.com>
2016-06-21 17:27:49 +0200

    Change url paths in default data

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ const projects = [
 	{
 		"id": "1",
 		"title": "Galaxy Goose",
-		"imgSrc": "/public/galaxygoose1.png",
+		"imgSrc": "public/galaxygoose1.png",
 		"descShort": "Interactive WebGL application",
 		"descLong": "An experimental WebGL application where you can create and customize planets interactively.",
 		"image": "assets/img/galaxygoose1.png",
@@ -14,7 +14,7 @@ const projects = [
 	{
 		"id": "2",
 		"title": "SPH fluid sim",
-		"imgSrc": "/public/watersim.jpg",
+		"imgSrc": "public/watersim.jpg",
 		"descShort": "3D water simulation",
 		"descLong": "Water simulation using a Smoothed Particle Hydrodynamics method, governered by Navier-Stokes equations. The implementation is in C++ with rendering through OpenGL and particle calculations with OpenCL. The simulation is in real-time with properties adjustable by a small GUI.",
 		"teamDesc": "Team of five",
@@ -25,7 +25,7 @@ const projects = [
 	{
 		"id": "3",
 		"title": "Reverbify",
-		"imgSrc": "/public/reverbify1.png",
+		"imgSrc": "public/reverbify1.png",
 		"descShort": "Reverb simulation app",
 		"descLong": "An app built as part of a sound physics course. The user can upload or record a sound and combine it with a chosen environment's echo effect.",
 		"teamDesc": "Team of three",
@@ -36,7 +36,7 @@ const projects = [
 	{
 		"id": "4",
 		"title": "Trappans Personalportal",
-		"imgSrc": "/public/portalen.png",
+		"imgSrc": "public/portalen.png",
 		"descShort": "Club staff portal",
 		"descLong": "Staff portal for a local pub/club. I've worked on dynamic JavaScript- and some php-backend.",
 		"teamDesc": "Team of four",
@@ -47,7 +47,7 @@ const projects = [
 	{
 		"id": "5",
 		"title": "Mapus",
-		"imgSrc": "/public/mapus1.png",
+		"imgSrc": "public/mapus1.png",
 		"descShort": "Study tool app",
 		"descLong": "An Android app for students to use for quickly communicating where to meet to study, with the help of interactive maps.",
 		"teamDesc": "Team of five",
