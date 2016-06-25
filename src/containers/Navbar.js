9bd891af4d891c7988314e1a2ff4c8851cd2ad0b
codingInSpace <jonathan.grangien@gmail.com>
2016-06-25 20:13:22 +0200

    Apply active class css style no navbar items

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from "react-router";
 import { connect } from 'react-redux';
+import { Grid } from 'react-bootstrap';
 
 class Navbar extends Component {
   render() {
   	let activeClass = this.props.activeClass;
-  	const contactClass = activeClass === "contact" ? "active" : "";
-  	const projectsClass = activeClass === "projects" ? "active" : "";
-  	const topClass = activeClass === "top" ? "active" : "";
+  	const contactClass = activeClass === "contact" ? "active" : "inactive";
+  	const projectsClass = activeClass === "projects" ? "active" : "inactive";
+  	const topClass = activeClass === "top" ? "active" : "inactive";
 		return (
 			<div className="router-navbar">
-				<span className={contactClass}>
-					<Link to={{ to: "/", hash: "#contact" }}> Contact </Link>
-				</span>
-				<span className={projectsClass}>
-					<Link to={{ to: "/", hash: "#projects" }}> Projects </Link>
-				</span>
-				<span className={topClass}>
-					<Link to={{ to: "/", hash: "#top" }}> Top </Link>
-				</span>
+				<Grid>
+					<span className={contactClass}>
+						<Link to={{ to: "/", hash: "#contact" }}> Contact </Link>
+					</span>
+					<span className={projectsClass}>
+						<Link to={{ to: "/", hash: "#projects" }}> Projects </Link>
+					</span>
+					<span className={topClass}>
+						<Link to={{ to: "/", hash: "#top" }}> Top </Link>
+					</span>
+				</Grid>
 			</div>
 		);
 	}
