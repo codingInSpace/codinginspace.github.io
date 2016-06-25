2266fdcaeb9368111f8d1d5aab235a143b81a521
codingInSpace <jonathan.grangien@gmail.com>
2016-06-25 19:12:23 +0200

    Implement navbar items active class update from state

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from "react-router";
+import { connect } from 'react-redux';
 
 class Navbar extends Component {
   render() {
+  	let activeClass = this.props.activeClass;
+  	const contactClass = activeClass === "contact" ? "active" : "";
+  	const projectsClass = activeClass === "projects" ? "active" : "";
+  	const topClass = activeClass === "top" ? "active" : "";
 		return (
 			<div className="router-navbar">
-				<span><Link to={{ to: "/", hash: "#contact" }}>Contact</Link></span>
-				<span><Link to={{ to: "/", hash: "#projects" }}>Projects</Link></span>
-				<span><Link to={{ to: "/", hash: "#top" }}>Top</Link></span>
+				<span className={contactClass}>
+					<Link to={{ to: "/", hash: "#contact" }}> Contact </Link>
+				</span>
+				<span className={projectsClass}>
+					<Link to={{ to: "/", hash: "#projects" }}> Projects </Link>
+				</span>
+				<span className={topClass}>
+					<Link to={{ to: "/", hash: "#top" }}> Top </Link>
+				</span>
 			</div>
 		);
 	}
 }
 
-export default Navbar 
+const mapStateToProps = (state) => {
+	return { 
+		activeClass: state.activeClass 
+	};
+}
+
+
+export default connect(mapStateToProps, null)(Navbar)
