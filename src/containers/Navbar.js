b1cbf0bdf78f2fcaca2d897f01ff244079185e88
codingInSpace <jonathan.grangien@gmail.com>
2016-06-25 20:35:48 +0200

    Let navbar be transparent when active item is top

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import { Link } from "react-router";
 import { connect } from 'react-redux';
 import { Grid } from 'react-bootstrap';
+import classNames from 'classnames';
 
 class Navbar extends Component {
   render() {
-  	let activeClass = this.props.activeClass;
+
+  	const activeClass = this.props.activeClass;
   	const contactClass = activeClass === "contact" ? "active" : "inactive";
   	const projectsClass = activeClass === "projects" ? "active" : "inactive";
   	const topClass = activeClass === "top" ? "active" : "inactive";
+
+  	let navClasses = classNames({
+  		"router-navbar": true,
+  		"navtop": activeClass === "top"
+		});
+
 		return (
-			<div className="router-navbar">
+			<div className={navClasses}>
 				<Grid>
 					<span className={contactClass}>
 						<Link to={{ to: "/", hash: "#contact" }}> Contact </Link>
