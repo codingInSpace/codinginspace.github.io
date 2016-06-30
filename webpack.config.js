4096cebfbacb8449056336664ba225cfdf49c61e
codingInSpace <jonathan.grangien@gmail.com>
2016-06-30 16:32:37 +0200

    Include es7 support

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				loader: 'babel-loader',
-				query: { presets: [ 'es2015', 'react' ] }
+				query: { presets: [ 'es2015', 'stage-0', 'react' ] }
 			},
 			{
 				test: /\.scss$/,
